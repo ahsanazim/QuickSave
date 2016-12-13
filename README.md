09f910b340439c84a714907ab30306020d2e690d
AhsanAzim <ahsan-azim@live.com>
2016-12-12 23:19:13 -0500

    closing, pinning, muting all working

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 ## chromeUtil
 
-A chrome extension!
+A chrome extension that provides some productivity gains in chrome via
+fancy keybindings and the like.
 
 Currently supports keybindings for:
 - Muting / un-muting current tab (Cmd-Shift-M)
+- Mute / un-mute all other tabs (Cmd+Shift+L)
+- pin / unpin current tab (Cmd+Shift+P)
+- Close all other abs (Cmd+Shift+K)
+
+NOTE: keybindings also need to be set manually from the bottom of `chrome://extensions`
+page.  
+This is, unfortunately, unavoidable since latest versions of chrome.
 
 
 TODO:
 - change the logo
-- Mute / un-mute all other tabs (Command+Shift+L)
-- pin / unpin current tab (Command+Shift+P)
-- Close all other abs (Command+Shift+K)
+- something that uses the ability to open another tab?
+- maybe let the user decide, via a gui, what keybindings should apply where (doesn't
+sound too appealing though, since the keybindings are already limited in scope)
--- background.js
+++ background.js
@@ -1,38 +1,64 @@
-// note -- keybinding needs to be set manually as well from the
-// bottom of the chrome://extensions page
-
 chrome.commands.onCommand.addListener(function(command) {
 
   if (command == "mute-this-tab") {
+    // get the current tab
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       // Toggle the muted status
       var current = tabs[0]
       chrome.tabs.update(current.id, {'muted': !current.mutedInfo.muted });
       });
   }
+
   if (command == "mute-other-tabs") {
-    // may not work because tabs can be in other windows but still active:
-    // chrome.tabs.query({ active: false }, function(tabs) {
-    //   // Toggle the muted status
-    //   console.log({ ...tabs });
-    //   var current = tabs[0]
-    //   chrome.tabs.update(current.id, {'muted': !current.mutedInfo.muted });
-    //   });
+    let currTabUrl = '';
+    // get the current tab
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+      currTab = tabs[0];        // only ever returns array of size 1
+      currTabUrl = currTab.url;     // record current tab's url
+      // get all tabs
+      chrome.tabs.query({},(allTabs) => {
+        // iterate through them
+        allTabs.forEach((curr,val,idx) => {
+          // if it isn't our current tab
+          if (curr.url != currTabUrl) {
+            // mute it
+            chrome.tabs.query({url: curr.url}, function(tabs) {
+              // Toggle the muted status
+              var current = tabs[0]
+              chrome.tabs.update(current.id, {'muted': !current.mutedInfo.muted });
+              });
+          }
+        });
+      });
+    });
   }
+
   if (command == "close-other-tabs") {
-
-
+    let currTabUrl = '';
+    // get the current tab
+    chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+      currTab = tabs[0];        // only ever returns array of size 1
+      currTabUrl = currTab.url;     // record current tab's url
+      // get all tabs
+      chrome.tabs.query({}, (allTabs) => {
+        // iterate through them
+        allTabs.forEach((curr,val,idx) => {
+          // if it isn't our current tab
+          if (curr.url != currTabUrl) {
+            // close it
+            chrome.tabs.remove(curr.id);
+          }
+        });
+      });
+    });
   }
-  if (command == "pin-this-tab") {
 
+  if (command == "pin-this-tab") {
+    // get our currently focused tab
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      // Toggle the pinned status
+      var current = tabs[0]
+      chrome.tabs.update(current.id, {'pinned': !current.pinned });
+      });
   }
 });
-
-
-
-// TODO
-// include other utilites,
-// - muting all other tabs
-// - closing all other tabs
-// - pinning current tab
-// think about changing name. chromeKeyBinder?
